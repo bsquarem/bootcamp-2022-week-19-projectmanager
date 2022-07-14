affa9df8a38f005d03d897cb733d7f1287f5e9d2
Flavio Copes <flavio@flaviocopes.com>
2022-07-14 10:26:45 +0200

    Dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
 import Head from 'next/head'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const router = useRouter()
+
+  const { data: session, status } = useSession()
+
+  const loading = status === 'loading'
+
+  if (loading) {
+    return null
+  }
+
+  if (session) {
+    router.push('/dashboard')
+    return
+  }
+
   return (
     <div>
       <Head>
@@ -12,25 +29,14 @@ export default function Home() {
       <div className='text-center '>
         <h1 className='mt-10 font-extrabold text-2xl'>Project Manager</h1>
 
-        <div className='grid sm:grid-cols-2'>
-          <div>
-            <h2 className='mt-10 font-bold'>Project #1</h2>
+        <p className='mt-10'>The best way to manage your projects!</p>
 
-            <ol className='mt-4 list-inside list-decimal'>
-              <li>TODO 1</li>
-              <li>TODO 2</li>
-              <li>TODO 3</li>
-            </ol>
-          </div>
-          <div>
-            <h2 className='mt-10 font-bold'>Project #2</h2>
+        <p className='mt-10'>Free 7 days trial then just $19.99/m</p>
 
-            <ol className='mt-4 list-inside list-decimal'>
-              <li>TODO 1</li>
-              <li>TODO 2</li>
-              <li>TODO 3</li>
-            </ol>
-          </div>
+        <div className='mt-10'>
+          <a className='bg-black text-white px-5 py-2' href='/api/auth/signin'>
+            Log in
+          </a>
         </div>
       </div>
     </div>
